773ed82f722e8c8f065c3abd653b0c384197b4b9
Bhaskar Goyal <bhaskargoyal24@gmail.com>
2016-03-04 20:38:55 +0530

    Added a app called home for home url

--- src/purse/urls.py
+++ src/purse/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^money', 'money.views.index', name='money'),
+    url(r'^$', 'home.views.index', name='home'),
 ]
