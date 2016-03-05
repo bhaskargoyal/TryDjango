296f8cfc362e15ece85ec33daca8701c10052383
Bhaskar Goyal <bhaskargoyal24@gmail.com>
2016-03-05 12:19:40 +0530

    Added forms.py in home app

--- src/home/models.py
+++ src/home/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class SignUp(models.Model):
+	email = models.EmailField()
+	name = models.CharField(max_length=120)
+	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+
+	def __unicode__(self):
+		return self.email
